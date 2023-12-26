f25c9bd226f6ad99eedd414f085f3ad7cdfbfe39
Huthifa Alqasim <huthifa2013@gmail.com>
2023-12-26 16:54:33 +0200

    alfred bot assignment

--- Fundamentals/alfred_bot_assignment/AlfredQuotes.java
+++ Fundamentals/alfred_bot_assignment/AlfredQuotes.java
@@ -8,22 +8,40 @@ public class AlfredQuotes {
     
     public String guestGreeting(String name) {
         // YOUR CODE HERE
-        return "placeholder for guest greeting return string";
+        String helloName = String.format("Hello, %s. Lovely to see you.", name);
+        return helloName;
     }
     
     public String dateAnnouncement() {
         // YOUR CODE HERE
-        return "placeholder for date announcement return string";
+        Date date = new Date();
+        String nowDate = String.format("It is currently %s.", date);
+        return nowDate;
     }
     
     public String respondBeforeAlexis(String conversation) {
         // YOUR CODE HERE
-        return "for snarky response return string";
+        int alexis = conversation.indexOf("Alexis"); 
+        int alfred = conversation.indexOf("Alfred"); 
+        if (alexis != -1){
+            return "Right away";
+        } else if (alfred != -1){
+            return "As you wish";
+        } else {
+            return "Right. And with that I shall retire";
+        }
+        
     }
     
 	// NINJA BONUS
 	// See the specs to overload the guestGreeting method
         // SENSEI BONUS
         // Write your own AlfredQuote method using any of the String methods you have learned!
+
+    public String guestGreeting(String name , String dayPeriod){
+        // YOUR CODE HERE
+        String GuestGreetingDayPeriod = String.format("Good %s, %s. Lovely to see you.", dayPeriod,name );
+        return GuestGreetingDayPeriod;
+    }
 }
 
