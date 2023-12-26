5649cc20cc55b229923c5f0b1022f1a9a6cdf439
Huthifa Alqasim <huthifa2013@gmail.com>
2023-12-26 22:33:53 +0200

    test only

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+    String[] fruits = new String[5];
+    
+    for (int i=0 ; i < fruits.length; i++) {
+        System.out.println(fruits[i]);
+    }
+    System.out.println(fruits);
+    }
+}
