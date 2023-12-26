2cdf19188e73a668c1fbc753588968a5638eff22
Huthifa Alqasim <huthifa2013@gmail.com>
2023-12-26 11:51:25 +0200

    alfred bot assignment

--- Fundamentals/first_java_program_assignment/FirstJavaProgram.java
+++ Fundamentals/first_java_program_assignment/FirstJavaProgram.java
@@ -1,7 +1,13 @@
 public class FirstJavaProgram{
     public static void main(String[] args){
-        System.out.println("My name is Coding Dojo");
-        System.out.println("I am 100 years old");
+        String myName = "Coding Dojo";
+        double myAge = 100;
+        String homeTown = "Burbank";
+
+        System.out.println("My name is " + myName);
+        String dd = String.format("I am %.8f years old", myAge);
+        System.out.println(dd);
         System.out.println("My hometown is Burbank, CA");
+
     } 
 }
