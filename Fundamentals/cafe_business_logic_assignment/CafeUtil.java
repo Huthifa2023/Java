9a8914eac95a3c1b6632a485bc23b25266482a3c
Huthifa Alqasim <huthifa2013@gmail.com>
2023-12-27 14:19:01 +0200

    cafe_business_logic

---
+++ Fundamentals/cafe_business_logic_assignment/CafeUtil.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class CafeUtil {
+    public int getStreakGoal() {
+        int sum = 0;
+        int numWeeks = 10;
+        for(int i=1; i <= numWeeks; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public double getOrderTotal(double[] prices) {
+        double total = 0;
+        for(double i : prices) {
+            total += i;
+        }
+        return total;
+    }
+
+    public void displayMenu(ArrayList<String> menuItems) {
+        for(int i = 0; i < menuItems.size(); i++) {
+            System.out.println(i + "  " + menuItems.get(i));
+        }
+    }
+
+
+    public void addCustomer(ArrayList<String> customers) {
+
+        System.out.println("Please enter your name:");
+        String username = System.console().readLine();
+        System.out.println("hello" +" "+ username);
+        customers.add(username);
+        int peopleAhead = customers.size();
+        System.out.println(String.format("There are %d people in front of you", peopleAhead));
+        System.out.println(customers);
+    }
+}
